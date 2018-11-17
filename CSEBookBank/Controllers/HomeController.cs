6fd0540a1c3178a5ed7921d519b212282c881825
rida0910 <43719846+rida0910@users.noreply.github.com>
2018-11-17 14:24:43 +0500

    Add project files.

---
+++ CSEBookBank/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSEBookBank.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MyBooks()
+        {
+            return View();
+        }
+
+        public ActionResult History()
+        {
+            return View();
+        }
+        public ActionResult Notifications()
+        {
+            return View();
+        }
+    }
+}

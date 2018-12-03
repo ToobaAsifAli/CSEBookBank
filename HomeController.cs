b60d53cc4aff206258bcf96e1ba4fe7e28f85efe
Faiqa507 <45161693+Faiqa507@users.noreply.github.com>
2018-12-03 07:26:55 +0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,45 @@
+﻿using CSEBookBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSEBookBank.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private CSEBookBankDbEntities db = new CSEBookBankDbEntities();
+        public ActionResult Index()
+        {
+            var books = db.Books;
+            return View(books.ToList());
+        }
+
+        public ActionResult IssueBook()
+        {
+
+            return View();
+        }
+
+        public ActionResult MyBooks()
+        {
+          
+            return View();
+        }
+
+        public ActionResult History()
+        {
+            return View();
+        }
+        public ActionResult Notifications()
+        {
+            return View();
+        }
+        public ActionResult returnBook()
+        {
+            return View();
+        }
+    }
+}

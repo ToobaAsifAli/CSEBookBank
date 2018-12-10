804c61e8dd7c9ce2d1008196de02337e503e2630
ToobaAsifAli <34550387+ToobaAsifAli@users.noreply.github.com>
2018-12-09 16:34:18 -0800

    images

--- CSEBookBank/Controllers/LibrarianController.cs
+++ CSEBookBank/Controllers/LibrarianController.cs
@@ -10,43 +10,117 @@ namespace CSEBookBank.Controllers
 {
     public class LibrarianController : Controller
     {
-        private CSEBookBankDbEntities db = new CSEBookBankDbEntities();
+       private CSEBookBankDbEntities db = new CSEBookBankDbEntities();
         // GET: Librarian
         public ActionResult Index()
         {
             var stds = db.students;
             return View(stds.ToList());
         }
-       
+
+        //public ActionResult ViewBooks()
+        //{
+        //    var books = db.Books;
+        //    return View(books.ToList());
+        //}
+
+        //public ActionResult AddBook()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public ActionResult AddBook([Bind(Include = "Title,Author,Edition,BookID,ImagePath,Description")] Book book)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Books.Add(book);
+        //        db.SaveChanges();
+        //        return RedirectToAction("ViewBooks");
+        //    }
+        //    return View();
+        //}
 
 
-        public ActionResult ViewBooks()
-        {
-            var books = db.Books;
-            return View(books.ToList());
-        }
-
+        [HttpGet]
         public ActionResult AddBook()
         {
             return View();
         }
 
 
+        //[HttpPost]
+        //public ActionResult AddBook([Bind(Include = "Title,Author,Edition,BookID,ImagePath")] Book book)
+        //{
+
+        //        string filename = Path.GetFileNameWithoutExtension(book.ImageFile.FileName);
+        //        string extension = Path.GetExtension(book.ImageFile.FileName);
+        //        filename = filename + DateTime.Now.ToString("yymmssfff") + extension;
+        //        book.ImagePath = "~/Image/" + filename;
+        //        filename = Path.Combine(Server.MapPath("~/Image/"), filename);
+        //        book.ImageFile.SaveAs(filename);
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Books.Add(book);
+        //            db.SaveChanges();
+        //            return RedirectToAction("ViewBooks");
+
+        //    }
+        //    ModelState.Clear();
+        //    return View();
+        //}
+
 
 
 
         [HttpPost]
-        public ActionResult AddBook([Bind(Include = "Title,Author,Edition,BookID,ImagePath,Description")] Book book)
+        public ActionResult AddBook([Bind(Include = "Title,Author,Edition,BookID,ImagePath")] Book book)
         {
-            if (ModelState.IsValid)
+
+            try
             {
-                db.Books.Add(book);
-                db.SaveChanges();
-                return RedirectToAction("ViewBooks");
+                string filename = Path.GetFileNameWithoutExtension(book.ImageFile.FileName);
+                string extension = Path.GetExtension(book.ImageFile.FileName);
+                filename = filename + DateTime.Now.ToString("yymmssfff") + extension;
+                book.ImagePath = "~/Image/" + filename;
+                filename = Path.Combine(Server.MapPath("~/Image"), filename);
+                book.ImageFile.SaveAs(filename);
+                using (CSEBookBankDbEntities db = new CSEBookBankDbEntities())
+                {
+                    if (db.Books.Any(x => x.Title == book.Title))
+                    {
+                        ViewBag.DuplicateMessage = "CityName already exist.";
+                        return View(book);
+                    }
+                    db.Books.Add(book);
+                    db.SaveChanges();
+                }
+
             }
-            return View();
+            catch (Exception ex)
+            {
+                return View(ex);
+
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Successful";
+            return RedirectToAction("ViewBooks");
+
+
         }
 
+
+	[HttpGet]
+        public ActionResult ViewBooks(int id)
+        {
+            Book book = new Book();
+            using (CSEBookBankDbEntities db = new CSEBookBankDbEntities())
+            {
+                book = db.Books.Where(x => x.BookID == id).FirstOrDefault();
+
+            }
+            return View(book);
+        }
         public ActionResult RemoveBook(int? id)
         {
 
